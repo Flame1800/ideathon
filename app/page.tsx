ce54bfdac1586dd4b6ede6637ddec94447bcee9b
Flame1800 <ruslan08022001@gmail.com>
2023-11-23 16:39:25 +0500

    add icon and title

--- app/page.tsx
+++ app/page.tsx
@@ -6,32 +6,35 @@ import Form from "../src/compoents/Form";
 // https://codepen.io/Sidstumple/pen/RwGJOow
 export default function Home() {
   return (
-    <main className="flex flex-col items-center ">
-      <div className="bg-[url('/img/noize.png')] w-full h-[100vh] absolute z-10"></div>
-      <header className="w-[440px] logo flex flex-col items-center mt-[70px]">
+    <>
+
+      <main className="flex flex-col items-center ">
+        <div className="bg-[url('/img/noize.png')] w-full h-[100vh] absolute z-10"></div>
+        <header className="w-[440px] logo flex flex-col items-center mt-[70px]">
+          <Image
+            className="logo max-w-[440px]"
+            width={440}
+            height={32}
+            src="/img/logo.svg"
+            alt="logo"
+          />
+          <div className="sub-title font-primary text-orange text-xl">
+            use your brain
+          </div>
+        </header>
+        <section className="mt-[70px] w-screen">
+          <EventDate />
+          <Title />
+        </section>
+        <Form />
         <Image
-          className="logo max-w-[440px]"
-          width={440}
-          height={32}
-          src="/img/logo.svg"
-          alt="logo"
+          src="/img/ellipse.png"
+          className="absolute bottom-0 z-1"
+          width={1112}
+          height={1014}
+          alt="ellipse"
         />
-        <div className="sub-title font-primary text-orange text-xl">
-          use your brain
-        </div>
-      </header>
-      <section className="mt-[70px] w-screen">
-        <EventDate />
-        <Title />
-      </section>
-      <Form />
-      <Image
-        src="/img/ellipse.png"
-        className="absolute bottom-0 z-1"
-        width={1112}
-        height={1014}
-        alt="ellipse"
-      />
-    </main>
+      </main>
+    </>
   );
 }
